0372ebd64eb9248999b2e4c62923466ea9562c7a
Pravin <115397005+PravinElangovan@users.noreply.github.com>
2023-05-07 13:28:31 +0530

    Update script.js

--- profile/script.js
+++ profile/script.js
@@ -1,9 +1,10 @@
 var user;
+var users;
 var currentUser = sessionStorage.getItem('username')
 console.log(currentUser)
 
 function getUserById(email) {
-    const users = JSON.parse(localStorage.getItem('users'));
+    users = JSON.parse(localStorage.getItem('users'));
     user = users.find(user => user.email === email);
     console.log(user)
     return user;
@@ -11,20 +12,44 @@ function getUserById(email) {
 
 getUserById(currentUser);
 
+function getUserDetails(user){
+    document.getElementById('my-profile').innerHTML=`
+    <h1>My Profile </h1>
+    <div>First Name : ${user.firstName}</div>
+    <div>Last Name : ${user.lastName}</div>
+    <div>Email : ${user.email}</div>
+    `
+}
+getUserDetails(user);
+
+const editButton = document.getElementById('edit-info');
 
 
-const saveButton = document.getElementById('save');
-var firstName = document.getElementById('fname').value=user.firstName;
-var lastName = document.getElementById('lname').value=user.lastName;
-var password = document.getElementById('old-pass').value=user.password;
 
 
- var newfirstName=document.getElementById('fname').value;
- var newLastName=document.getElementById('lname').value
 
-saveButton.addEventListener('click',()=>{
-    console.log("Button clicked")
-})
+ document.getElementById('change-password').addEventListener('click', () => {
+    var currentPassword = document.getElementById('old-pass').value;
+    var newPassword = document.getElementById('new-pass').value;
+    var confirmPassword = document.getElementById('confirm-pass').value;
+
+    if (currentPassword !== user.password) {
+        alert("Incorrect current password!");
+        return;
+    }
+
+    if (newPassword !== confirmPassword) {
+        alert("New password and confirm password do not match!");
+        return;
+    }
+
+    // update the user's password in the local storage
+    user.password = newPassword;
+    localStorage.setItem('users', JSON.stringify(users));
+
+    alert("Password updated successfully!");
+});
+
 document.getElementById('logout').addEventListener('click', () => {
     sessionStorage.clear();
     console.log("Logged Out Successfully")
